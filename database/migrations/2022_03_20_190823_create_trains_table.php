180716dfaeed8b2bce70aaf449995e3289e15ef9
MatteoFilippini <matteofilippini8@gmail.com>
2022-03-20 20:43:46 +0100

    created trains table

---
+++ database/migrations/2022_03_20_190823_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 50);
+            $table->string('start_stat', 30);
+            $table->string('end_stat', 30);
+            $table->date('start_time');
+            $table->date('end_time');
+            $table->char('num_train', 5);
+            $table->tinyInteger('carriages');
+            $table->boolean('in_time')->default(true);
+            $table->boolean('is_delete')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
